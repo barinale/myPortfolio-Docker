da41360e26ee8f9597e06d327a1638bed5535491
barinale <barinale08@gmail.com>
2024-10-14 23:25:43 +0100

    Update MyProject.js

--- src/Components/Portfolio/MyProject.js
+++ src/Components/Portfolio/MyProject.js
@@ -5,7 +5,7 @@ export const MyProject = () => {
 
     const ProjectList =[
     {id:1,UrlCode:'https://github.com/barinale/movie-list',UrlView:'https://movielistimdb.netlify.app/',ImageUrl:'./assestsO/MovieLst.png',TechnologyUser:[1,2,3,4,5,6,7]},
-    {id:2,UrlCode:'https://github.com/barinale/openData',UrlView:'https://barinale.github.io/openData/',ImageUrl:'./assestsO/OpenDAta.png',TechnologyUser:[1,2,3,4]},
+    {id:2,UrlCode:'https://github.com/barinale/openData',UrlView:'https://barinale.github.io/ResponsiveWebDesign-openData/',ImageUrl:'./assestsO/OpenDAta.png',TechnologyUser:[1,2,3,4]},
     {id:3,UrlCode:'https://github.com/barinale/2cSolution',UrlView:'https://barinale.github.io/2cSolution/',ImageUrl:'./assestsO/Solution.png',TechnologyUser:[1,2,3,4]},
     {id:4,UrlCode:'https://github.com/barinale/ecomercewebsite',UrlView:'https://barinale.github.io/ecomercewebsite/',ImageUrl:'./assestsO/ecomrc.png',TechnologyUser:[1,2,3,4]},
     {id:5,UrlCode:'https://github.com/barinale/busnis',UrlView:'https://barinale.github.io/busnis/',ImageUrl:'./assestsO/busnin.png',TechnologyUser:[1,2,3]},
